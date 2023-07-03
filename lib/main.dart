cc47379d577959f7aed858c4ac36ef11ee8458f2
mxsibay022 <mxsibay022@student.wethinkcode.co.za>
2023-07-03 07:04:35 +0200

    added firebase funct but still to add services

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import 'TypefSignUp.dart';
 import 'TypeOfUser.dart';
 
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
